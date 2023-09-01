5f0b15ffd8df69aa2759ec95357fca1107fa9c0e
andresguezz <jarodriguez3203@misena.edu.co>
2023-09-01 16:27:27 -0500

    Object to iterrate between jobs

--- src/components/CardEmpleo.vue
+++ src/components/CardEmpleo.vue
@@ -1,27 +1,34 @@
 <template>
-  <b-card class="d-flex align-items-center">
-    <img src="../assets/logo.png" alt="Imagen" class="custom-image">
-    <div class="flex-grow-1">
-      <div class="d-flex justify-content-between">
-        <h1 class="mb-0">Empleo</h1>
-        <div class="d-flex">
-          <b-button variant="primary">Tecnologia 1</b-button>
-          <b-button variant="primary">Tecnologia 2</b-button>
-          <b-button variant="primary">Tecnologia 3</b-button>
+  <div>
+    <b-card
+      v-for="(empleo, index) in empleos"
+      :key="index"
+      class="d-flex align-items-center"
+    >
+      <img src="../assets/logo.png" alt="Imagen" class="custom-image">
+      <div class="flex-grow-1">
+        <div class="d-flex justify-content-between">
+          <h1 class="mb-0">{{ empleo.titulo }}</h1>
+          <div class="d-flex">
+            <b-button
+              v-for="tecnologia in empleo.tecnologias"
+              :key="tecnologia"
+              variant="primary"
+            >{{ tecnologia }}</b-button>
+          </div>
+        </div>
+        <div class="d-flex justify-content-between mt-3">
+          <div class="d-flex flex-column">
+            <p class="mb-0">Empresa: {{ empleo.empresa }}</p>
+            <p class="mb-0">Lugar: {{ empleo.lugar }}</p>
+            <p class="mb-0">Sueldo: {{ empleo.sueldo }}</p>
+            <p class="mb-0">Vacantes: {{ empleo.vacantes }}</p>
+            <p class="mb-0">Fecha: {{ empleo.fecha }}</p>
+          </div>
         </div>
       </div>
-      <div class="d-flex justify-content-between mt-3">
-        <div class="d-flex flex-column">
-          <p class="mb-0">Empresa</p>
-          <p class="mb-0">Lugar</p>
-          <p class="mb-0">Sueldo</p>
-          <p class="mb-0">Vacantes</p>
-          <p class="mb-0">Fecha</p>
+    </b-card>
   </div>
-</div>
-
-    </div>
-  </b-card>
 </template>
 
 <script>
@@ -31,6 +38,40 @@ export default {
   components: {
     BCard,
     BButton
+  },
+  data() {
+    return {
+      empleos: [
+        {
+          titulo: 'Junior dev',
+          tecnologias: ['JavaScript', 'React'],
+          empresa: 'Code heaven',
+          lugar: 'Latin America',
+          sueldo: '$5000',
+          vacantes: 3,
+          fecha: '3 days ago'
+        },
+        {
+          titulo: 'Senior Dev',
+          tecnologias: ['Python', 'Django'],
+          empresa: 'La empresa',
+          lugar: 'Colombia',
+          sueldo: '$160000',
+          vacantes: 1,
+          fecha: '5 days ago'
+        },
+        {
+          titulo: 'E-consultant ',
+          tecnologias: ['Result Oriented ', 'Problem Analysis'],
+          empresa: 'Future company',
+          lugar: 'Anywhere',
+          sueldo: '$60000',
+          vacantes: 2,
+          fecha: '3 days ago'
+        },
+        
+      ]
+    };
   }
 }
 </script>
